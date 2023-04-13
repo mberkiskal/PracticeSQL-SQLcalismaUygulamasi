b83cfa266c9dd46e14ea9f365da47b46b5c9896c
Musa Berk İşkal <m.berkiskal@gmail.com>
2023-04-13 21:51:15 +0300

    SQL Panel with Windows Form App
    You can perform all sql operations from this panel. The application first connects to the 'master' database. Then you can select the database and table you want to work with the combobox. And you can your query. Tüm SQL işlemlerini bu panelden gerçekleştirebilirsiniz. Uygulama önce 'master' veri tabanına bağlanır. Sonrasında combobox ile çalışmak istediğiniz veri tabanını ve tabloyu seçebilirsiniz. Ve sorgunuzu çalıştırabilirsiniz.

---
+++ p12SQLcalismaUygulamasi/p12SQLcalismaUygulamasi/Form1.Designer.cs
@@ -0,0 +1,284 @@
+﻿namespace p12SQLcalismaUygulamasi
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cmbDatabases = new System.Windows.Forms.ComboBox();
+            this.cmbTables = new System.Windows.Forms.ComboBox();
+            this.rchQuery = new System.Windows.Forms.RichTextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnExecute = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnWhatis = new System.Windows.Forms.Button();
+            this.btnDownload = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.SteelBlue;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(-1, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1349, 100);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century", 39.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(263, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(750, 64);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "ADMINISTER DATABASES!";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(13, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(97, 74);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dataGridView1);
+            this.groupBox1.Location = new System.Drawing.Point(532, 106);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(803, 439);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Output:";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 25);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(797, 411);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(32, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 24);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Database:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(65, 116);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 24);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Table:";
+            // 
+            // cmbDatabases
+            // 
+            this.cmbDatabases.FormattingEnabled = true;
+            this.cmbDatabases.Location = new System.Drawing.Point(149, 61);
+            this.cmbDatabases.Name = "cmbDatabases";
+            this.cmbDatabases.Size = new System.Drawing.Size(182, 32);
+            this.cmbDatabases.TabIndex = 4;
+            this.cmbDatabases.SelectedIndexChanged += new System.EventHandler(this.cmbDatabases_SelectedIndexChanged);
+            // 
+            // cmbTables
+            // 
+            this.cmbTables.FormattingEnabled = true;
+            this.cmbTables.Location = new System.Drawing.Point(149, 113);
+            this.cmbTables.Name = "cmbTables";
+            this.cmbTables.Size = new System.Drawing.Size(182, 32);
+            this.cmbTables.TabIndex = 5;
+            this.cmbTables.SelectedIndexChanged += new System.EventHandler(this.cmbTables_SelectedIndexChanged);
+            // 
+            // rchQuery
+            // 
+            this.rchQuery.Location = new System.Drawing.Point(16, 222);
+            this.rchQuery.Name = "rchQuery";
+            this.rchQuery.Size = new System.Drawing.Size(451, 152);
+            this.rchQuery.TabIndex = 6;
+            this.rchQuery.Text = "";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnClear);
+            this.groupBox2.Controls.Add(this.btnExecute);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.rchQuery);
+            this.groupBox2.Controls.Add(this.cmbTables);
+            this.groupBox2.Controls.Add(this.cmbDatabases);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Location = new System.Drawing.Point(12, 106);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(495, 501);
+            this.groupBox2.TabIndex = 7;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Process:";
+            // 
+            // btnExecute
+            // 
+            this.btnExecute.BackColor = System.Drawing.Color.SteelBlue;
+            this.btnExecute.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnExecute.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnExecute.ForeColor = System.Drawing.SystemColors.Window;
+            this.btnExecute.Location = new System.Drawing.Point(161, 380);
+            this.btnExecute.Name = "btnExecute";
+            this.btnExecute.Size = new System.Drawing.Size(122, 44);
+            this.btnExecute.TabIndex = 8;
+            this.btnExecute.Text = "Execute!";
+            this.btnExecute.UseVisualStyleBackColor = false;
+            this.btnExecute.Click += new System.EventHandler(this.btnExecute_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 195);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 24);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Query:";
+            // 
+            // btnWhatis
+            // 
+            this.btnWhatis.BackColor = System.Drawing.Color.SteelBlue;
+            this.btnWhatis.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnWhatis.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnWhatis.ForeColor = System.Drawing.SystemColors.Window;
+            this.btnWhatis.Location = new System.Drawing.Point(532, 563);
+            this.btnWhatis.Name = "btnWhatis";
+            this.btnWhatis.Size = new System.Drawing.Size(376, 44);
+            this.btnWhatis.TabIndex = 9;
+            this.btnWhatis.Text = "What is MSSQL Server?";
+            this.btnWhatis.UseVisualStyleBackColor = false;
+            this.btnWhatis.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnDownload
+            // 
+            this.btnDownload.BackColor = System.Drawing.Color.SteelBlue;
+            this.btnDownload.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnDownload.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnDownload.ForeColor = System.Drawing.SystemColors.Window;
+            this.btnDownload.Location = new System.Drawing.Point(956, 563);
+            this.btnDownload.Name = "btnDownload";
+            this.btnDownload.Size = new System.Drawing.Size(376, 44);
+            this.btnDownload.TabIndex = 10;
+            this.btnDownload.Text = "Download MSSQL Server!";
+            this.btnDownload.UseVisualStyleBackColor = false;
+            this.btnDownload.Click += new System.EventHandler(this.btnDownload_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.BackColor = System.Drawing.Color.SteelBlue;
+            this.btnClear.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnClear.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnClear.ForeColor = System.Drawing.SystemColors.Window;
+            this.btnClear.Location = new System.Drawing.Point(161, 439);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(122, 44);
+            this.btnClear.TabIndex = 9;
+            this.btnClear.Text = "Clear!";
+            this.btnClear.UseVisualStyleBackColor = false;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.ClientSize = new System.Drawing.Size(1347, 619);
+            this.Controls.Add(this.btnDownload);
+            this.Controls.Add(this.btnWhatis);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Franklin Gothic Medium", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MSSQL";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cmbDatabases;
+        private System.Windows.Forms.ComboBox cmbTables;
+        private System.Windows.Forms.RichTextBox rchQuery;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnExecute;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnWhatis;
+        private System.Windows.Forms.Button btnDownload;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ p12SQLcalismaUygulamasi/p12SQLcalismaUygulamasi/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace p12SQLcalismaUygulamasi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        void db()
+        {
+            SqlConnection con = new SqlConnection("Data Source=DESKTOP-23T2RIK\\SQLEXPRESS;Initial Catalog=master;Integrated Security=True");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("select name from sys.databases",con);
+            SqlDataReader dr= cmd.ExecuteReader();
+
+            while (dr.Read())
+            {
+                cmbDatabases.Items.Add(dr[0].ToString());
+            }
+            con.Close();
+        }
+        
+        void table()
+        {
+            SqlConnection conn = new SqlConnection("Data Source=DESKTOP-23T2RIK\\SQLEXPRESS;Initial Catalog=" + cmbDatabases.Text + ";Integrated Security=True");
+            conn.Open();
+            SqlCommand cmd1 = new SqlCommand("select * from sys.Tables", conn);
+            SqlDataReader dr1 = cmd1.ExecuteReader();
+            while (dr1.Read())
+            {
+                cmbTables.Items.Add(dr1[0].ToString());
+            }
+            conn.Close();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            db();
+            SqlConnection connection = new SqlConnection("Data Source=DESKTOP-23T2RIK\\SQLEXPRESS;Initial Catalog=" + cmbDatabases.Text + ";Integrated Security=True");
+        }
+
+        private void cmbDatabases_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            cmbTables.Items.Clear();
+            table();
+        }
+        private void cmbTables_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            rchQuery.Text="select * from "+ cmbTables.Text;
+        }
+
+        private void btnExecute_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlConnection sqlConnection = new SqlConnection("Data Source=DESKTOP-23T2RIK\\SQLEXPRESS;Initial Catalog=" + cmbDatabases.Text + ";Integrated Security=True");
+                sqlConnection.Open();
+                SqlCommand sqlCommand = new SqlCommand(rchQuery.Text, sqlConnection);
+                sqlCommand.ExecuteNonQuery();
+                sqlConnection.Close();
+
+                SqlDataAdapter da = new SqlDataAdapter("select * from " + " " + cmbTables.Text, sqlConnection);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                dataGridView1.DataSource = dt;
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Please Check Your Query And Try Again!", "Operation Failed!",MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://en.wikipedia.org/wiki/Microsoft_SQL_Server");
+        }
+
+        private void btnDownload_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://www.microsoft.com/en-us/sql-server/sql-server-downloads");
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            cmbDatabases.Text = "";
+            cmbTables.Text = "";
+            rchQuery.Text = "";
+        }
+    }
+}
